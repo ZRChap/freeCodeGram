8c36f3995219ee35e7fe34040fc9d88110dc4c89
Zack <zensai@protonmail.com>
2020-02-03 20:32:28 -0500

    laravel

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -9,27 +9,27 @@
         <div class="col-9 pt-5 pl-5">
             <div class="d-flex justify-content-between align-items-baseline">
             <h1>{{ $user->userName }}</h1>
-            <a href="#">Add New Post</a>
+            <a href="/p/create">Add New Post</a>
             </div>
+            <a href="/profile/{{ $user->id }}/edit">Edit Profile</a>
             <div class="d-flex">
-                <div class="pr-5"><strong>261</strong> posts</div>
+                <div class="pr-5"><strong>{{ $user->posts->count() }}</strong> posts</div>
                 <div class="pr-5"><strong>37.5k</strong> followers</div> 
                 <div class="pr-5"><strong>233</strong> following</div>
             </div>
-            <div class="pt-3" style="font-weight: bold">{{ $user->profile->title }}</div>
+            <div class="pt-4" style="font-weight: bold">{{ $user->profile->title }}</div>
             <div>{{ $user->profile->description }}</div>
             <div><a href="#">{{ $user->profile->url }}</div>
         </div>
     </div>
-    <div class="row pt-4">
-        <div class="col-4">
-            <img src="https://instagram.fmia1-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c0.111.928.928a/s640x640/82967728_222124025469517_5532436576170466152_n.jpg?_nc_ht=instagram.fmia1-1.fna.fbcdn.net&_nc_cat=101&_nc_ohc=-Gj6yghlXy0AX_31ije&oh=b4aa4c284a57d384451862b48a892bf9&oe=5EC3C23B" alt="" class="w-100">
+    <div class="row pt-5">
+    @foreach($user->posts as $post)
+        <div class="col-4 pb-4">
+            <a href="/p/{{ $post->id }}">
+            <img src="/storage/{{ $post->image }}" class="w-100">
+            </a>
         </div>
-        <div class="col-4">
-            <img src="https://instagram.fmia1-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c0.17.782.782a/s640x640/83882094_189688875767629_4724838109548338712_n.jpg?_nc_ht=instagram.fmia1-1.fna.fbcdn.net&_nc_cat=101&_nc_ohc=3fCURgyOH_AAX-YDjz3&oh=f59a8356efcea21e4299ff56f2046ee2&oe=5EBD4006" alt="" class="w-100">
-        </div>
-        <div class="col-4" class="w-100">
-            <img src="https://instagram.fmia1-2.fna.fbcdn.net/v/t51.2885-15/e35/c95.0.560.560a/83685533_642392923240008_2402831147948054363_n.jpg?_nc_ht=instagram.fmia1-2.fna.fbcdn.net&_nc_cat=100&_nc_ohc=BDv2G-RkoyAAX8Fa0WT&oh=1ea41c065b7966b51097db59274b728e&oe=5ED4D373" alt="" class="w-100">
-        </div>
+    @endforeach
+    </div>
 </div>
 @endsection
